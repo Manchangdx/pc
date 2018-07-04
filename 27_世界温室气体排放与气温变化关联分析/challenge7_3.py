3d460cb022d78444894556af10e3b885f886b0ea
Manchangdx <1195581533@qq.com>
2018-07-04 12:35:00 +0800

    修改挑战 27 的代码

--- 27_世界温室气体排放与气温变化关联分析/challenge7_3.py
+++ 27_世界温室气体排放与气温变化关联分析/challenge7_3.py
@@ -10,16 +10,19 @@ def climate_plot():
 
     gt = pd.read_excel('GlobalTemperature.xlsx')
     gt = gt.iloc[:, [1, 4]].set_index(pd.to_datetime(gt.Date))
+    gt.fillna(0, inplace=True)
     gt_y = gt.resample('y').mean()['1990': '2010']
     gt_q = gt.resample('q').mean()
     df = pd.concat([gt_y.set_index(data.index), data], axis=1)
     df = df.apply(lambda x: (x-x.min())/(x.max()-x.min()))
 
     fig, axes = plt.subplots(nrows=2, ncols=2)
-    ax1 = df.plot(kind='line', figsize=(16, 9), ax=axes[0, 0])
+    ax1 = df.plot(kind='line', figsize=(16, 9), ax=axes[0, 0], xticks=df.index)
     ax1.set_xlabel('Years')
     ax1.set_ylabel('Values')
-    ax1.xaxis.set_major_formatter(ticker.FormatStrFormatter("%d"))
+    ax1.set_xticklabels(df.index, rotation=90)
+    # 设置主刻度格式（使用旧的％来格式化刻度的值，％d 表示整数）
+    # ax1.xaxis.set_major_formatter(ticker.FormatStrFormatter("%d"))
     ax2 = df.plot(kind='bar', figsize=(16, 9), ax=axes[0, 1])
     ax2.set_xlabel('Years')
     ax2.set_ylabel('Values')
@@ -28,10 +31,9 @@ def climate_plot():
     ax3.set_ylabel('Temperature')
     # 所谓核密度分布图，就是各个温度值(x轴)所占总数据比例(y轴)的分布图
     ax4 = gt_q.plot(kind='kde', figsize=(16, 9), ax=axes[1, 1])
-    ax4.set_xlabel('Quarters')
-    ax4.set_ylabel('Temperature')
+    ax4.set_xlabel('Values')
+    ax4.set_ylabel('Values')
     plt.show()
     return fig
 
 print(climate_plot())
-
