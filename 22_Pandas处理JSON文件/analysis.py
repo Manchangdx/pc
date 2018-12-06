22e46e4dfa4c3e498c92cd845d30114ab2cc064f
Manchangdx <1195581533@qq.com>
2018-12-06 10:56:19 +0800

    修改挑战 22 的代码

--- 22_Pandas处理JSON文件/analysis.py
+++ 22_Pandas处理JSON文件/analysis.py
@@ -1,5 +1,7 @@
 def analysis(file, user_id):
     import pandas as pd
     df = pd.read_json(file)
-    s = df[df.user_id==user_id].minutes
-    return s.count(), s.sum()
+    if user_id in list(df.user_id):
+        s = df[df.user_id==user_id].minutes
+        return s.count(), s.sum()
+    return 0, 0
