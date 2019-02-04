87a3573a05c71f1572fa52657bd897e7d78fae18
Manchangdx <1195581533@qq.com>
2019-02-04 16:22:31 +0800

    增加实验 10 练习题参考答案

---
+++ 00_练习题参考答案/10_常用模块/创建多个文件夹及文件/add_file.py
@@ -0,0 +1,9 @@
+import os
+
+os.makedirs('syl/A')
+os.makedirs('syl/B')
+os.makedirs('syl/C')
+os.system('touch syl/A/__init__.py')
+os.system('touch syl/B/__init__.py')
+os.system('touch syl/C/__init__.py')
+os.system('touch syl/__init__.py')
