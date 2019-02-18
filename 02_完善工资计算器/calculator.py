cf913e9aa772b9c45c2644a3398577054e11af73
Manchangdx <1195581533@qq.com>
2019-02-18 11:47:30 +0800

    修改挑战 2 的代码

--- 02_完善工资计算器/calculator.py
+++ 02_完善工资计算器/calculator.py
@@ -8,26 +8,26 @@ def cal(salary):
     except ValueError:
         print('Parameter Error')
         exit()
-    start_point = 3500  # 起征点
+    start_point = 5000  # 起征点
     social_insurance_point = 0.08 + 0.02 + 0.005 + 0.06  # 社保比例
     # 需要缴税的那部分工资
     tax_part_salary = salary * (1-social_insurance_point) - start_point  
     if tax_part_salary <= 0:
-        tax = 0
-    elif tax_part_salary <= 1500:
+        tax = 0 
+    elif tax_part_salary <= 3000:
         tax = tax_part_salary * 0.03
-    elif tax_part_salary <= 4500:
-        tax = tax_part_salary * 0.1 - 105
-    elif tax_part_salary <= 9000:
-        tax = tax_part_salary * 0.2 - 555
+    elif tax_part_salary <= 12000:
+        tax = tax_part_salary * 0.1 - 210 
+    elif tax_part_salary <= 25000:
+        tax = tax_part_salary * 0.2 - 1410
     elif tax_part_salary <= 35000:
-        tax = tax_part_salary * 0.25 - 1005
+        tax = tax_part_salary * 0.25 - 2660
     elif tax_part_salary <= 55000:
-        tax = tax_part_salary * 0.3 - 2755
+        tax = tax_part_salary * 0.3 - 4410
     elif tax_part_salary <= 80000:
-        tax = tax_part_salary * 0.35 - 5505
+        tax = tax_part_salary * 0.35 - 7160
     else:
-        tax = tax_part_salary * 0.45 - 13505
+        tax = tax_part_salary * 0.45 - 15160
     after_tax_salary = salary * (1-social_insurance_point) - tax
     return '{:.2f}'.format(after_tax_salary)
 
