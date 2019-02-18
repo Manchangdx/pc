c6da00cc98f6d04c95a7ebd2892940217938d2b3
Manchangdx <1195581533@qq.com>
2019-02-18 12:20:21 +0800

    修改挑战 4 的代码

--- 04_多进程工资计算器/calculator.py
+++ 04_多进程工资计算器/calculator.py
@@ -38,25 +38,25 @@ def compute(salary):
         social_insurance_salary = config['JiShuL'] * config['s']
     if salary > config['JiShuH']:
         social_insurance_salary = config['JiShuH'] * config['s']
-    start_point = 3500  # 起征点
+    start_point = 5000  # 起征点
     # 需要缴税的那部分工资
     tax_part_salary = salary - social_insurance_salary - start_point
     if tax_part_salary <= 0:
         tax = 0 
-    elif tax_part_salary <= 1500:
+    elif tax_part_salary <= 3000:
         tax = tax_part_salary * 0.03
-    elif tax_part_salary <= 4500:
-        tax = tax_part_salary * 0.1 - 105 
-    elif tax_part_salary <= 9000:
-        tax = tax_part_salary * 0.2 - 555 
+    elif tax_part_salary <= 12000:
+        tax = tax_part_salary * 0.1 - 210 
+    elif tax_part_salary <= 25000:
+        tax = tax_part_salary * 0.2 - 1410
     elif tax_part_salary <= 35000:
-        tax = tax_part_salary * 0.25 - 1005
+        tax = tax_part_salary * 0.25 - 2660
     elif tax_part_salary <= 55000:
-        tax = tax_part_salary * 0.3 - 2755
+        tax = tax_part_salary * 0.3 - 4410
     elif tax_part_salary <= 80000:
-        tax = tax_part_salary * 0.35 - 5505
+        tax = tax_part_salary * 0.35 - 7160
     else:
-        tax = tax_part_salary * 0.45 - 13505
+        tax = tax_part_salary * 0.45 - 15160
     # 税后工资
     after_tax_salary = salary - social_insurance_salary - tax
     return [salary, format(social_insurance_salary, '.2f'), format(tax, '.2f'),
