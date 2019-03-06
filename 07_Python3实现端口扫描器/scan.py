f0cb1ec0be0ae88d1a6aca939a755abed6be83e7
Manchangdx <1195581533@qq.com>
2019-03-06 14:15:17 +0800

    修改挑战 7 的代码

--- 07_Python3实现端口扫描器/scan.py
+++ 07_Python3实现端口扫描器/scan.py
@@ -1,34 +1,37 @@
 import sys, socket
 
 def scan():
-    l = sys.argv
+    args = sys.argv
     try:
-        host = l[l.index('--host')+1]
-        port = l[l.index('--port')+1]
+        host = args[args.index('--host')+1]
+        port = args[args.index('--port')+1]
         assert len(host.split('.')) == 4
         if '-' in port:
-            a, b = port.split('-')
-            assert int(a) < int(b)
-            port = range(int(a), int(b)+1)
+            start, end = port.split('-')
+            assert int(start) < int(end)
+            ports = range(int(start), int(end)+1)
         else:
-            port = [int(port)]
+            ports = [int(port)]
     except (ValueError, IndexError, AssertionError):
         print('Parameter Error')
         exit()
-    l = []
-    for i in port:
-        s = socket.socket()  # 创建套接字
-        s.settimeout(0.1)    # 设置套接字操作的超时期，timeout 是一个浮点数，单位是秒
+
+    open_ports = []
+    s = socket.socket()     # 创建套接字
+    s.settimeout(0.1)       # 设置套接字操作的超时期，参数为浮点数，单位是秒
+
+    for port in ports:
         # connect 方法的作用是初始化 TCP 服务器连接
-        # 这个方法是 connect 方法的扩展版本, 出错时返回出错码, 而不是抛出异常
-        # 出错码是非零数字
-        if s.connect_ex((host, i)) == 0:  
-            l.append(i)
-            print(i, 'open')
+        # connect_ex 是 connect 的扩展版本，接收主机＋端口号元组作为参数
+        # 出错时返回出错码，而不是抛出异常，出错码是非零数字
+        if s.connect_ex((host, port)) == 0:
+            open_ports.append(port)
+            print(port, 'open')
         else:
-            print(i, 'close')
-        s.close()
-    print('Complted scan. Opening ports at {}'.format(l))
+            print(port, 'close')
+
+    s.close()
+    print('Complted scan. Opening ports at {}'.format(open_ports))
 
 if __name__ == '__main__':
     scan()
