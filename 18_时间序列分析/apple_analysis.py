37e7aa1b99ae65017e6950693a462da41325c6e1
Manchangdx <1195581533@qq.com>
2019-03-15 16:00:54 +0800

    增加挑战 18 的代码

--- 18_时间序列分析/apple_analysis.py
+++ 18_时间序列分析/apple_analysis.py
@@ -1,7 +1,7 @@
+import pandas as pd
+
 def quarter_volume():
-    import pandas as pd
-    from pandas import Series
     data = pd.read_csv('apple.csv')
-    s = Series(list(data.Volume), index=pd.to_datetime(data.Date))
+    s = pd.Series(list(data.Volume), index=pd.to_datetime(data.Date))
     second_volume = s.resample('q').sum().sort_values()[-2]
     return second_volume
