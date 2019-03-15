88bfb1f54d6c0d672db89b4a5f897afb25c3512b
Manchangdx <1195581533@qq.com>
2019-03-15 15:40:12 +0800

    增加挑战 16 的代码

--- 99_backup/22_Pandas处理JSON文件/analysis.py
+++
@@ -1,7 +0,0 @@
-def analysis(file, user_id):
-    import pandas as pd
-    df = pd.read_json(file)
-    if user_id in list(df.user_id):
-        s = df[df.user_id==user_id].minutes
-        return s.count(), s.sum()
-    return 0, 0
