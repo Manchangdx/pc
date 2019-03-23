c70fbe726b15d2f44bce0408e64936c57eb25eca
Manchangdx <1195581533@qq.com>
2019-03-23 12:58:25 +0800

    增加挑战 20 的代码

--- 20_各国CO2排放量与GDP总值关联分析/challenge2.py
+++ 20_各国CO2排放量与GDP总值关联分析/challenge2.py
@@ -2,19 +2,21 @@ import numpy as np, pandas as pd, matplotlib.pyplot as plt
 
 def co2_gdp_plot():
     data = pd.read_excel('ClimateChange.xlsx')
-    gdp = data[data['Series code']=='NY.GDP.MKTP.CD'].set_index('Country code').iloc[:, 5:]
-    co2 = data[data['Series code']=='EN.ATM.CO2E.KT'].set_index('Country code').iloc[:, 5:]
+    gdp = data[data['Series code']=='NY.GDP.MKTP.CD'].set_index(
+            'Country code').iloc[:, 5:]
+    co2 = data[data['Series code']=='EN.ATM.CO2E.KT'].set_index(
+            'Country code').iloc[:, 5:]
     gdp.replace({'..': pd.np.nan}, inplace=True)
     co2.replace({'..': pd.np.nan}, inplace=True)
     gdp = gdp.fillna(method='ffill', axis=1).fillna(method='bfill', axis=1)
     co2 = co2.fillna(method='ffill', axis=1).fillna(method='bfill', axis=1)
-    df = pd.concat([co2.sum(axis=1), gdp.sum(axis=1)], axis=1)
+    # gdp 和 co2 里都有整行 NaN 值，求和后变成 0
+    df = pd.concat([co2.sum(1), gdp.sum(1)], 1)
     df.columns = ['GDP-SUM', 'CO2-SUM']
-    df.fillna(0, inplace=True)  # 这步操作是为了兼容 pandas 0.20 版本，高于此版本无需这步
     df = df.apply(lambda x:(x-x.min())/(x.max()-x.min()))  # 归一化
     strick_labels, labels_position = [], []  # 刻度标签名称和标签的横坐标
     for i in range(len(df)):
-        if df.index[i] in ['USA', 'CHN', 'FRA', 'RUS', 'GBR']:
+        if df.index[i] in {'USA', 'CHN', 'FRA', 'RUS', 'GBR'}:
             strick_labels.append(df.index[i])
             labels_position.append(i)
     fig = plt.subplot()
@@ -25,4 +27,5 @@ def co2_gdp_plot():
     plt.show()
     return fig, np.round(df['CHN':'CHN'].values, 3).tolist()[0]
 
-print(co2_gdp_plot())
+if __name__ == '__main__':
+    print(co2_gdp_plot())
