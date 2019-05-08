0d34090182cd6221cd09893739b1be4a468bd5ef
Manchangdx <1195581533@qq.com>
2019-05-08 23:45:23 +0800

    修改挑战【基于 Selenium 与 PhantomJS 实现爬虫】

--- 12_基于Selenium与PhantomJS实现爬虫/spider.py
+++ 12_基于Selenium与PhantomJS实现爬虫/spider.py
@@ -1,40 +1,36 @@
 import json
-from functools import reduce
+import time
 from selenium import webdriver
 from scrapy.http import HtmlResponse
 from selenium.webdriver import ActionChains
-from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.by import By
 
 def spider():
-    driver = webdriver.Chrome()                 # 启动谷歌驱动，打开浏览器
+    driver = webdriver.Chrome()    # 启动谷歌驱动，打开浏览器
     url = 'https://www.shiyanlou.com/courses/427'
-    driver.get(url)                             # 打开待爬取页面
-    page = 1 
+    driver.get(url)                # 打开待爬取页面
     result = []
     while True:
-        WebDriverWait(driver, 10).until(        # 显式等待 10 秒，默认轮询 0.5 秒
-            EC.text_to_be_present_in_element(   # 设置条件
-                (By.XPATH, '//ul[@class="pagination"]/li[@class="active"]'),
-                str(page)                       # 上一行代码块里是否有这一行的字段
-            )
-        )
+        driver.implicitly_wait(3)  # 隐式等待 3 秒
         html = driver.page_source
         response = HtmlResponse(url=url, body=html.encode('utf-8'))
-        for i in response.css('div.comment-item-wrapper'):
+        for comment in response.css('div.col-11'):
             d = {
-                'username': i.css('a.username::text').extract_first().strip(),
-                'content': reduce(lambda a, b: a+b, (i.css('p::text').extract()))
+                'username': comment.css('a::text').extract_first().strip(),
+                'content': ' '.join([i.strip() for i in list(comment.css(
+                    'div.content::text').extract())])
             }
             result.append(d)
-        if response.css('div.comment-box li.disabled.next-page'):
-            break   # 如果有 class 属性值为 disalbed 的 li 标签，表示没有下一页了
-        page += 1
-        # chromedirver 无法自动定位到当前页面未显示区域，下面两行代码起到定位作用
-        ac = driver.find_element_by_css_selector('div.comment-box li.next-page a')
+        # 如果第二个 li 标签 class 属性值包含 disalbed 字段，表示没有下一页了
+        if 'disabled' in response.xpath('(//li[contains'
+            '(@class, "page-item")])[2]/@class').extract_first():
+            break
+        # chromedirver 无法自动定位到当前页面未显示区域，下面三行代码起到定位作用
+        # 定位到第二个 li 标签，也就是“下一页”那个按钮
+        ac = driver.find_element_by_xpath(
+            '(//li[contains(@class, "page-item")])[2]')
         ActionChains(driver).move_to_element(ac).perform()
-        driver.find_element_by_css_selector('div.comment-box li.next-page').click()
+        time.sleep(1)  # 等待按钮加载
+        ac.click()     # 点击下一页按钮
     driver.quit()
     with open('comments.json', 'w') as f:
         json.dump(result, f)
